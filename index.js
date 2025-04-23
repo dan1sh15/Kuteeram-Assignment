0a2f445fe3bf127434b9879db99bd1db8f0033c0
danish <danishqayyum9793@gmail.com>
2025-04-24 02:20:56 +0530

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const connectDB = require('./config/db');
-require('dotenv').config();
+const express = require("express");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
 // Route files
-const authRoutes = require('./routes/authRoutes/index');
-const serviceRoutes = require('./routes/serviceRoutes/index');
-const bookingRoutes = require('./routes/bookingRoutes/index');
+const authRoutes = require("./routes/authRoutes/index");
+const serviceRoutes = require("./routes/serviceRoutes/index");
+const bookingRoutes = require("./routes/bookingRoutes/index");
 
 const app = express();
 
@@ -13,18 +13,20 @@ const app = express();
 app.use(express.json());
 
 // Mount routers
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/services', serviceRoutes);
-app.use('/api/v1/bookings', bookingRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/services", serviceRoutes);
+app.use("/api/v1/bookings", bookingRoutes);
 
 // Connect to database
 connectDB();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running on port ${PORT}`
-  )
-);
+app.get("/", (req, res) => {
+  return res.send({
+    success: true,
+    message: "Welcome to Booking service provider Application!!!",
+  });
+});
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
